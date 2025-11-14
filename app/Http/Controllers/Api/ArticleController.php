07e68c43fffa3a0493cfabee4a3b52f00f91625c
Thiago Trancoso <thiagostrn@gmail.com>
2025-11-14 17:39:03 -0300

    Aula 6

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
     public function show(Article $article): ArticleResource
     {
         return ArticleResource::make($article);
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
+
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
